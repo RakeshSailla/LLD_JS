ae759ee31dc706e52bc807ce1c94ea357d3cfd62
Rakesh <rakesh.sailla@gmail.com>
2025-08-30 18:04:34 +0530

    Array methods

---
+++ Class-8(Introduction to JS)/Class-9(Arrays, Strings, Objects)/arrays.js
@@ -0,0 +1,31 @@
+let arr = [1, true, null, 'Hello']
+console.log(arr)
+
+let cars = ["Ford", "Tata", "Mahindra"]
+console.log(cars)
+
+//length
+console.log(cars.length)
+
+//Array methods:
+//.push
+cars.push('Maruti Suzuki')
+console.log(cars)
+
+//.pop
+cars.pop()
+console.log(cars)
+
+//unshift
+cars.unshift('Volkwagen')
+console.log(cars)
+
+//shift
+cars.shift()
+console.log(cars)
+
+//includes
+console.log(cars.includes("BMW"))
+
+//indexOf
+console.log(cars.indexOf("Urus"))
--- Class-8(Introduction to JS)/script.js
+++
@@ -1,6 +0,0 @@
-console.log("Welcome to Introduction to JavaScript")
-
-// Comments
-// How to add comment?
-
-//Variables in JS
